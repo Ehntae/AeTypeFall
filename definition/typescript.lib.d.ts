083866acbd733674789b5e90ee7fc7f9b6c32145
Aeomi <Aeomei@gmail.com>
2018-04-21 06:42:39 -0400

    Comment out unneeded imports

---
+++ definition/typescript.lib.d.ts
@@ -0,0 +1,13 @@
+/// <reference no-default-lib="true"/>
+
+
+/// <reference path="../node_modules/typescript/lib/lib.es2015.core.d.ts" />
+/// <reference path="../node_modules/typescript/lib/lib.es2015.collection.d.ts" />
+/// <reference path="../node_modules/typescript/lib/lib.es2015.generator.d.ts" />
+//// <reference path="../node_modules/typescript/lib/lib.es2015.promise.d.ts" />
+//// <reference path="../node_modules/typescript/lib/lib.es2015.iterable.d.ts" />
+//// <reference path="../node_modules/typescript/lib/lib.es2015.proxy.d.ts" />
+//// <reference path="../node_modules/typescript/lib/lib.es2015.reflect.d.ts" />
+//// <reference path="../node_modules/typescript/lib/lib.es2015.symbol.d.ts" />
+//// <reference path="../node_modules/typescript/lib/lib.es2015.symbol.wellknown.d.ts" />
+/// <reference path="../node_modules/typescript/lib/lib.es5.d.ts" />
