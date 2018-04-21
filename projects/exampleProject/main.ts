2e89959778b6bd4a9b4dd949b2eca512540fcdc6
Aeomi <Aeomei@gmail.com>
2018-04-21 06:42:03 -0400

    ExampleProject serves to test

--- projects/exampleProject/main.ts
+++ projects/exampleProject/main.ts
@@ -1,19 +1,46 @@
 // @ts-ignore
-if (CLIENT) { return; }
+if (CLIENT) return;
 
 function f(x: number) {
-    const str = `f-d a thing: ${toString(x)}`;
+	const str = `f-d a thing: ${toString(x)}`;
 
-    const ar: Array<string> = [];
+	const arr: Primative[] = new Array<Primative>();
 
-    return str;
+	arr.push(5);
+	arr.push("five");
+	arr.push(true);
+
+	return arr;
 }
 
-print(f(6));
+const str = f(6);
 
-export class C1 {
+const ar: IColor[] = [Color(2, 2, 2, 1)];
 
+const colorsOnly = (color: IColor): void => {
+	print(color.r);
+};
+
+
+ar.forEach(colorsOnly);
+
+//@ts-ignore
+str.forEach(print); // ! Error => check output
+str.forEach(() => print);
+// tslint:disable-next-line:variable-name
+str.forEach(_x => print);
+str.forEach(x => print);
+
+
+if (true) throw "testError1";
+if (true) throw Error("testError2");
+
+const div0 = 1 / 0;
+// tslint:disable-next-line:use-isnan
+if (div0 === NaN) print("nan");
+if (isNaN(div0)) print('fnan'); // See the resulting screen too
+
+class MyClass {
+	private readonly memba: string;
+	public member: string;
 }
-
-// This should error
-export class C2 {}
