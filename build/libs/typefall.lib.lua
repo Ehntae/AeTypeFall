5a83996832e6845beb59cfe90cf852e1d05132ce
Aeomi <Aeomei@gmail.com>
2018-04-25 16:45:31 -0400

    Revert lib implementation

--- build/libs/typefall.lib.lua
+++ build/libs/typefall.lib.lua
@@ -4,14 +4,4 @@
 
 ]]
 
-std = {}
-
-std.contains = function(tbl, value)
-    for i, v in ipairs(tbl) do
-        if (v == value) then
-            return true
-        end
-    end
-    
-    return false
-end
+-- ...
